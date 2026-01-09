4424e0c342a7cec67974d35f61edf318759f7f0a
HanielUlises <hanielulises2003@gmail.com>
2026-01-08 23:55:24 -0600

    Sum type

--- examples/simple_variant.cpp
+++ examples/simple_variant.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 
+#include "overload.hpp"
+
 int main() {
     using Value = std::variant<int, double, std::string>;
 
@@ -9,7 +11,7 @@ int main() {
     Value b = 3.1415;
     Value c = std::string("hello");
 
-    auto print = std::overload{
+    auto print = overload{
         [](int i)              { std::cout << "int:    " << i << '\n'; },
         [](double d)           { std::cout << "double: " << d << '\n'; },
         [](const std::string& s){ std::cout << "string: " << s << '\n'; }
