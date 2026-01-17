6d2a746289b2dcca889d103e495956aa2f22bcc0
HanielUlises <hanielulises2003@gmail.com>
2026-01-16 22:13:22 -0600

    Visitation

--- examples/generic_lambda.cpp
+++ examples/generic_lambda.cpp
@@ -12,4 +12,11 @@ int main() {
         else if constexpr(std::is_same_v<decltype(x), char>)
             std::cout << x << "c\n";
     }, v0);
+
+    std::variant<int, std::string> v0 {1};
+    auto *s = std::get_if<std::string>(&v0);
+
+    if(s) {
+        std::cout << "Value retrieved" << '\n';
+    }
 }
