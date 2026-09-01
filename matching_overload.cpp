d93ce0c1dd9e199511288ff4e7e74ede6a6a3d74
HanielUlises <hanielulises2003@gmail.com>
2026-08-31 21:15:10 -0600

    Overload set

--- matching_overload.cpp
+++ matching_overload.cpp
@@ -1,7 +1,11 @@
 #include "overload.hpp"
 
 template <typename T>
-struct foo_float { T operator(float op)(return )};
+struct foo_float {
+    T operator()(float op) { return T{}; }
+};
 
 template <typename T>
-struct foo_char { T operator(char op)(return )};
+struct foo_char {
+    T operator()(char op) { return T{}; }
+};
