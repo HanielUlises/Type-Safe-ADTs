97f20f38fbeb14195ab205d7a4017318b866131d
HanielUlises <hanielulises2003@gmail.com>
2026-08-31 21:18:24 -0600

    Overload set

--- matching_overload.cpp
+++ matching_overload.cpp
@@ -1,11 +1,31 @@
+#include <iostream>
 #include "overload.hpp"
 
 template <typename T>
 struct foo_float {
-    T operator()(float op) { return T{}; }
+    T operator()(float op) {
+        return T{};
+    }
 };
 
 template <typename T>
 struct foo_char {
-    T operator()(char op) { return T{}; }
+    T operator()(char op) {
+        return T{};
+    }
 };
+
+using foo = overload_set<foo_float<float>, foo_char<char>>;
+
+int main() {
+    foo f{
+        foo_float<float>{},
+        foo_char<char>{}
+    };
+
+    auto x0 = f(0.f);
+    auto x1 = f('a');
+
+    std::cout << x0 << '\n';
+    std::cout << x1 << '\n';
+}
--- overload.hpp
+++ overload.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 template<typename A, typename B>
-struct overload_set : A , B {
+struct overload_set : A, B {
     using A::operator();
     using B::operator();
+
+    overload_set(A a, B b)
+        : A(a), B(b) {}
 };
 
 template<class... Ts>
