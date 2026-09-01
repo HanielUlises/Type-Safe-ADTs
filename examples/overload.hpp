005f25dad211e2d40f09a9f82d99c8fb0e10e654
HanielUlises <hanielulises2003@gmail.com>
2026-08-31 21:09:59 -0600

    Overload set

--- examples/overload.hpp
+++ examples/overload.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+template<typename A, typename B>
+struct overload_set : A , B {
+    using A::operator();
+    using B::operator();
+};
+
 template<class... Ts>
 struct overload : Ts... {
     using Ts::operator()...;
