c6a758aaa49b2b9cb97832fbd71c1c7ce6a8bc84
HanielUlises <hanielulises2003@gmail.com>
2026-09-06 23:07:01 -0600

    Based types

---
+++ category-theory/types.hpp
@@ -0,0 +1,18 @@
+template<class T>
+void unit(T) {}
+
+enum bool {
+    true,
+    false
+};
+
+template<class T>
+struct mempty;
+
+template<class T>
+T mappend(T, T) = delete;
+template<class M>
+concept Monoid = requires (M m) {
+    { mempty<M>::value() } -> std::same_as<M>;
+    { mappend(m, m) } -> std::same_as<M>;
+};
