0d864c6c6202691e9a17592019e49115f3390aa6
HanielUlises <hanielulises2003@gmail.com>
2026-09-14 19:56:11 -0600

    Semigroup

--- category-theory/types.hpp
+++ category-theory/types.hpp
@@ -1,4 +1,9 @@
+#pragma once
+
+#define TEST1
+
 #include <concepts>
+#include <set>
 
 template<class T>
 void unit(T) {}
@@ -13,3 +18,26 @@ concept Monoid = requires (M m) {
     { mempty<M>::value() } -> std::same_as<M>;
     { mappend(m, m) } -> std::same_as<M>;
 };
+
+template <typename T, typename BinOp>
+struct Semigroup {
+    std::set<T> S;
+    BinOp op;
+
+    T combine(T &a, T &b) {
+        return op(a, b);
+    }
+};
+
+#if defined(TEST1)
+struct Add {
+    int operator()(int a, int b) const {
+        return a + b;
+    }
+};
+
+Semigroup<int, Add> G{
+    {1, 2, 3},
+    Add{}
+};
+#endif 
